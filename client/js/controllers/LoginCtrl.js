14ba0746e17edc5b6922d4091a1b7b2c0d56e360
mahendran-aiyappan <mahendran.aiyappan@gmail.com>
2014-11-20 05:57:34 +0530

    Invalid username, password error message functionality

--- client/js/controllers/LoginCtrl.js
+++ client/js/controllers/LoginCtrl.js
@@ -1,13 +1,14 @@
-angular.module('LoginCtrl', []).controller('LoginController', function($scope, $http, $location, Auth) {
+angular.module('LoginCtrl', []).controller('LoginController', function($scope, $http, $location, Auth, $rootScope) {
 	$scope.user = {};
 	$scope.login = function() {
 		Auth.login({
 			username : $scope.user.username,
 			password : $scope.user.password
 		}, function(res) {
+			$rootScope.error = "";
 			$location.path('/home');
 		}, function(err) {
-			//$rootScope.error = "Failed to login";
+			$rootScope.error = "The email or password you entered is incorrect.";
 			$location.url('/');
 		});
 	};
